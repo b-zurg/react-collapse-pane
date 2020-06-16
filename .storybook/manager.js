31ea55fd36746df07f8807bd93913a091a1c871a
Buzurgmehr Arjmandi <buzurg.arjmandi@gmail.com>
2020-06-16 23:31:42 +0300

    feat: added knobs addon and reorganized stories

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,15 +1,29 @@
 import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
 import { create } from '@storybook/theming/create';
+import { configureActions } from '@storybook/addon-actions';
 
 addons.setConfig({
   theme: create({
     base: "dark",
-    appContentBg: 'white',
-    appBorderColor: 'grey',
-    barBg: 'white',
-    brandTitle: 'react-collapse-pane',
-    appBorderColor: 'silver',
 
+    appContentBg: 'white',
+    appBorderColor: 'silver',
+    barBg: 'white',
+    appBg: "#3e3e3e",
+
+    textColor: 'rgba(211,211,211,1.9)',
+    textInverseColor: 'rgba(211,211,211,1.9)',
+
+    brandTitle: 'react-collapse-pane',
+    brandUrl: "https://github.com/b-zurg/react-collapse-pane",
+    brandImage: "https://github.com/b-zurg/react-collapse-pane/raw/master/logo.svg?sanitize=true",
   }),
+});
+
+
+configureActions({
+  depth: 5,
+  // Limit the number of items logged into the actions panel
+  limit: 5,
 });
