7b1c1538fb9339161b17ad5aba749a651950446c
Buzurgmehr Arjmandi <buzurg.arjmandi@gmail.com>
2020-06-19 18:10:07 +0300

    fix: Multiple pane collapse initial size calculation incorrect

--- stories/InitialState.stories.tsx
+++ stories/InitialState.stories.tsx
@@ -51,6 +51,35 @@ storiesOf('Initial States', module)
       </VerticalSplitPane>
     );
   })
+  .add('Ltr, First Two Panes Collapsed', () => {
+    const verticalCollapseDirection = select(
+      'Vertical Direction',
+      { left: 'left', right: 'right' },
+      'left'
+    );
+    const shouldCollapse = boolean('Collapsable?', true);
+    const verticalCollapseOptions = shouldCollapse
+      ? {
+          beforeToggleButton: <Button>{verticalCollapseDirection === 'left' ? '⬅' : '➡'}</Button>,
+          afterToggleButton: <Button>{verticalCollapseDirection === 'left' ? '➡' : '⬅'}</Button>,
+          collapsedSize: 40,
+          collapseDirection: verticalCollapseDirection,
+        }
+      : undefined;
+    const VerticalSplitPane = (props: Omit<SplitPaneProps, 'split'>) => (
+      <SplitPane split={'vertical'} collapseOptions={verticalCollapseOptions} {...props}>
+        {props.children}
+      </SplitPane>
+    );
+
+    return (
+      <VerticalSplitPane collapsedSizes={[200, 200, null]}>
+        <div>This is a div</div>
+        <div>This is a second div</div>
+        <div>This is a third div</div>
+      </VerticalSplitPane>
+    );
+  })
   .add('Initial Sizes as Flex-Basis proportions', () => {
     return (
       <SplitPane split="vertical" initialSizes={[1, 2, 1]}>
