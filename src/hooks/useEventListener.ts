073a790ac8bc2cbaa623173ecf2b46d40c73c9f6
Buzurgmehr Arjmandi <buzurg.arjmandi@gmail.com>
2020-06-19 17:42:43 +0300

    fix: abort useEventListener.ts if component has not finished mounting

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -5,10 +5,12 @@ export function useEventListener<K extends keyof WindowEventMap>(
   listener?: (this: Window, ev: WindowEventMap[K]) => void
 ): void {
   useEffect(() => {
+    const abortController = new AbortController();
     if (!listener) return;
     window.addEventListener(type, listener);
     return (): void => {
       window.removeEventListener(type, listener);
+      abortController.abort();
     };
   }, [type, listener]);
 }
